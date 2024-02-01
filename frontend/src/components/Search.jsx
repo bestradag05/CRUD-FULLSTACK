994ac4acaad7625b78c41f7718eebd61e6eb1e98
bestradag05 <bestradag05@gmail.com>
2024-02-01 17:02:58 -0500

    Search Track

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,26 +1,61 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Select from 'react-select';
+import useAuth from '../hooks/useAuth';
 
 const Search = () => {
 
-    const [query, setQuery] = useState("");
+  const { spotifyToken } = useAuth();
+  const [query, setQuery] = useState("");
+  const [trackSearch, setTrackSearch] = useState([]);
 
-    const handleInputChange = (inputValue, { action }) => {
-        if (action === 'input-change') {
-          console.log('Valor de entrada:', inputValue);
-          // Puedes realizar acciones adicionales con el valor de entrada aquí
+  useEffect(() => {
+    if (query !== "") {
+      const searchTrack = async () => {
 
-          
+        try {
+          const response = await axios.get('https://api.spotify.com/v1/search', {
+            params: {
+              q: encodeURIComponent(query),
+              type: 'track',
+              market: 'ES', // Código de país ISO 3166-1 alfa-2, por ejemplo, 'ES' para España
+              limit: 5, // Número máximo de resultados por tipo de elemento
+              offset: 0, // Índice del primer resultado a devolver
+              include_external: 'audio', // Indica que el cliente puede reproducir contenido de audio alojado externamente
+            },
+            headers: {
+              Authorization: `Bearer ${spotifyToken}`,
+            },
+          });
+          console.log(response.data.tracks.items);
+
+          //Extraer los datos relevantes de la respuesta
+
+          const tracks = response.data.tracks.items.map(track => ({
+            label: `${track.name} - ${track.artists.map(artist => artist.name).join(', ')}`,
+            value: track.id
+          }));  
+
+          setTrackSearch(tracks);
+        } catch (error) {
+          console.log(error);
         }
-      };
 
-    return (
-        <div className="flex justify-center items-center p-5 mb-5">
-            <Select className='w-1/3' isSearchable
-                onInputChange={handleInputChange}
-                options={[ /* Tus opciones aquí */]} />
-        </div>
-    );
+      }
+
+      searchTrack();
+    }else{
+      setTrackSearch([]);
+    }
+
+  }, [query])
+
+  return (
+    <div className="flex justify-center items-center p-5 mb-5">
+      <Select className='w-1/3'
+        onInputChange={(inputValue) => setQuery(inputValue)}  options={trackSearch} />
+    </div>
+  );
 }
 
 export default Search;
